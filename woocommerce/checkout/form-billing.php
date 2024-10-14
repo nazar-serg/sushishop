71231f0f60d3580a5a752d1a19912c20e50e98ed
Nazarenko Sergey <nazarenkoenergodar@gmail.com>
2024-10-14 17:57:38 +0300

    Commit

--- woocommerce/checkout/form-billing.php
+++ woocommerce/checkout/form-billing.php
@@ -1,24 +1,7 @@
 <?php
-/**
- * Checkout billing information form
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/checkout/form-billing.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see     https://woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 3.6.0
- * @global WC_Checkout $checkout
- */
 
 defined( 'ABSPATH' ) || exit;
 ?>
-
 <div class="woocommerce-billing-fields">
 	<?php if ( wc_ship_to_billing_address_only() && WC()->cart->needs_shipping() ) : ?>
 
